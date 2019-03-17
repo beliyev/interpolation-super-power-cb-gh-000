03b84109daaf1f3cb895d5bcb9b42c3316bbee3f
Ilkin Babayev <beliyev1@std.beu.edu.az>
2019-03-17 18:00:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,11 +2,11 @@
 def display_rainbow(colors)
   colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
   display_rainbow = colors
-  puts display_rainbow(0)
-  puts display_rainbow(1)
-  puts display_rainbow(2)
-  puts display_rainbow(3)
-  puts display_rainbow(4)
-  puts display_rainbow(5)
-  puts display_rainbow(6)
+  puts display_rainbow(colors(0))
+  puts display_rainbow(colors(1))
+  puts display_rainbow(colors(2))
+  puts display_rainbow(colors(3))
+  puts display_rainbow(colors(4))
+  puts display_rainbow(colors(5))
+  puts display_rainbow(colors(6))
 end
